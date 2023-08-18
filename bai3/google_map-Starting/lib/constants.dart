a18628fd3c85201ccf9d248b75103a5d785aa7ee
dungle84620 <106216185+dungle84620@users.noreply.github.com>
2023-08-18 15:56:47 +0700

    18/08/2023

---
+++ bai3/google_map-Starting/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyDc7PnOq3Hxzq6dxeUVaY8WGLHIePl0swY";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
